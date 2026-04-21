b5dc6a0c6bd115bf2b971f7ff95925e29a260a75
Kankana-sde25 <kankanakarmakar29@gmail.com>
2026-04-21 14:48:30 +0530

    Update api.js
    updated the backend api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://spam-emial-fullstack-3.onrender.com';
 
 export const analyzeMessage = async (text) => {
   try {
